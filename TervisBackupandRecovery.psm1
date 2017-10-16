3adaa6003292ee32461f286b2b26c34811274de0
ChrisMagnuson <hourglasssand@gmail.com>
2017-10-16 14:50:18 -0400

    Move Definitions to separate file

--- TervisBackupandRecovery.psm1
+++ TervisBackupandRecovery.psm1
@@ -1,35 +1,5 @@
-﻿
-
-$TervisDPMServers= [pscustomobject][ordered]@{
-    DPmServerName="INF-SCDPM201601"
-    Description = "SQL/Exchange Datasources "
-    Role = "Primary"
-    Location = "HQ"
-},
-[pscustomobject][ordered]@{
-    DPmServerName="INF-SCDPM201602"
-    Description = "Fileserver,Data Drive Datasources"
-    Role = "Primary"
-    Location = "HQ"
-},
-[pscustomobject][ordered]@{
-    DPmServerName="INF-SCDPM201603"
-    Description = "Secondary Off-Site"
-    Role = "Secondary"
-    Location = "Peak10"
-},
-[pscustomobject][ordered]@{
-    DPmServerName="INF-DPM2016HQ1"
-    Description = "DPM 2016 Primary"
-    Role = "Primary"
-    Location = "HQ"
-}
-[pscustomobject][ordered]@{
-    DPmServerName="INF-DPM2016P10"
-    Description = "DPM 2016 Secondary"
-    Role = "Secondary"
-    Location = "Peak10"
-}
+﻿$ModulePath = (Get-Module -ListAvailable TervisBackupAndRecovery).ModuleBase
+. $ModulePath\Definition.ps1
 
 function Get-TervisDPMServers{
     param(
